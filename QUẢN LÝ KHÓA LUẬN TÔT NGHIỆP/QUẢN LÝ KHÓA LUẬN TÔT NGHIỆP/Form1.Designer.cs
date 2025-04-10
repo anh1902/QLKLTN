7e549ca3577fc2262cd783e8f3aeeb233a8ed5b1
anh <ngocanhtranthi42@gmail.com>
2025-04-10 15:32:52 +0700

    Thiết kế giao diện

--- QUẢN LÝ KHÓA LUẬN TÔT NGHIỆP/QUẢN LÝ KHÓA LUẬN TÔT NGHIỆP/Form1.Designer.cs
+++ QUẢN LÝ KHÓA LUẬN TÔT NGHIỆP/QUẢN LÝ KHÓA LUẬN TÔT NGHIỆP/Form1.Designer.cs
@@ -39,6 +39,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "ĐỀ TÀI ";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
--- QUẢN LÝ KHÓA LUẬN TÔT NGHIỆP/QUẢN LÝ KHÓA LUẬN TÔT NGHIỆP/Form1.cs
+++ QUẢN LÝ KHÓA LUẬN TÔT NGHIỆP/QUẢN LÝ KHÓA LUẬN TÔT NGHIỆP/Form1.cs
@@ -16,5 +16,11 @@ namespace QUẢN_LÝ_KHÓA_LUẬN_TÔT_NGHIỆP
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+        //thiết kế giao diện 
     }
 }
